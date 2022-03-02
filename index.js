5bd1785f01e479cca4f472e0264dd2fae99152d7
marcosvpa <marcos_andrade9@hotmail.com>
2022-03-02 14:20:16 -0300

    Add the promise function at readFile

--- index.js
+++ index.js
@@ -2,6 +2,30 @@ const chalk = require('chalk');
 const fs = require('fs');
 
 
-function pegaArquivo(caminhoDoArquivo) {
-  
+function trataErro(erro) {
+  throw new Error(erro);
 }
+
+function pegaArquivo(caminhoDoArquivo) {
+  const encoding = 'utf-8';
+  fs.promises
+  .readFile(caminhoDoArquivo, encoding)
+  .then((texto) =>  console.log(chalk.green(texto)))
+  .catch((erro) => trataErro(erro));
+}
+
+
+
+
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = 'utf-8';
+//   fs.readFile(caminhoDoArquivo, encoding,(erro,texto) =>{
+//     if (erro) {
+//       trataErro(chalk.red(erro.code,'Não há arquivo no caminho'));
+//     } else {
+//       console.log(chalk.green(texto));
+//     }
+    
+//   })
+// };
+pegaArquivo('./arquivos/texto1.md');
