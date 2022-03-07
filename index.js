ddaa41f564ac62dc78a158ecb08000e081b44bc7
marcosvpa <marcos_andrade9@hotmail.com>
2022-03-07 19:11:22 -0300

    Add the regex function

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const chalk = require('chalk');
 const fs = require('fs');
 
 
+
+
+function extraiLinks(texto) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const arrayResultados = [];
+  let temp;
+    while ((temp = regex.exec(texto)) !== null) {
+      arrayResultados.push({[temp[1]]:temp[2]});
+    }
+  return arrayResultados;
+}
+
 function trataErro(erro) {
   throw new Error(erro);
 }
@@ -11,7 +23,7 @@ async function pegaArquivo(caminhoDoArquivo) {
   const encoding = 'utf-8';
     try {
       const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-      console.log(chalk.green(texto));
+      console.log(extraiLinks(texto));
     } catch(erro) {
       trataErro(erro);
     }
